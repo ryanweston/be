89007b8d20ca62e160b88c71ac06e48c81c5a73d
Ryan Weston <ryanlweston@gmail.com>
2021-11-24 10:19:34 +0000

    start sound experiment + input tracking + generate

--- src/script.js
+++ src/script.js
@@ -10,7 +10,6 @@ import * as Tone from 'tone'
 
 const gui = new dat.GUI();
 
-const synth = new Tone.Synth().toDestination();
 
 const scene = new THREE.Scene()
 
@@ -18,19 +17,18 @@ let params = {
   size: 0.5,
   height: 64,
   width: 32,
-  fog: 0.002,
-  boundary: 2000
+  fog: 0.1,
+  boundary: 10
 }
 
 const bloom = {
   exposure: 2,
-  bloomStrength: 1.5,
-  bloomThreshold: 0,
-  bloomRadius: 0
+  bloomStrength: 4,
+  bloomThreshold: -2,
+  bloomRadius: 1
 };
 
 // Active inputs
-let active = []
 
 // Inputs 
 const input = { 
@@ -42,9 +40,11 @@ const input = {
   }
 }
 
+let active = []
+let states = [0,0] // 0 - Default, 1 - Adding or added, 2 - Released or releasing
 
 // Objects
-const group = new THREE.Group()
+const groups = [new THREE.Group(), new THREE.Group()]
 const cubes = []
 
 // Lights
@@ -54,6 +54,10 @@ scene.add( light );
 const spotLight = new THREE.SpotLight( 0xffffff );
 spotLight.position.set( 10, 10, 10 );
 scene.add( spotLight );
+
+// Grid helper
+// const gridHelper = new THREE.GridHelper( 10, 10 );
+// scene.add( gridHelper );
 
 
 
@@ -88,41 +92,92 @@ composer.addPass( bloomPass );
 
 // Check if we use that input
 function checkIfKey(key) {
-  const now = Tone.now()
   switch (key) {
     case 'KeyA': 
-      // trigger the attack immediately
-      synth.triggerAttack("C4", now)
       return true;
     case 'KeyD':
-      // trigger the attack immediately
-      synth.triggerAttack("G4", now)
       return true;
     default: 
       return false;   
   }
 }
 
+// SOUND EXPERIMENT
+
+// const synth = new Tone.Synth().toDestination();
+// const now = Tone.now()
+// const notesArr = ['G4', 'C2']
+// function notes (release) {
+//   console.log(active)
+
+//   if (release) {
+//     console.log('ACTIVE')
+//     let activeNotes = []
+ 
+//     active.forEach((item, index) => {
+//       if (item) activeNotes.push(notesArr[index])
+//       if (input) synth.triggerAttack(notesArr[index], now + (index + 1))
+//     })
+   
+//   } else {
+//     let activeNotes = []
+//     // for (let i = 0; i < active.length; i++) {
+//     //   console.log(input)
+//     //   if (input) synth.triggerAttack(notesArr[index], now)
+//     // }
+//     active.forEach((item, index) => {
+//       if (item) activeNotes.push(notesArr[index])
+//       // if (input) synth.triggerRelease(notesArr[index], now)
+//     })
+//     synth.triggerRelease(activeNotes, now)
+//   }
+// }
+
 // Event listeners
+// TRIGGER PRESS
 document.addEventListener('keydown', (e) => {
-  let keyCheck = checkIfKey(e.code);
-  if (keyCheck === true) {
-    active[e.code] = true
+  let isKey = checkIfKey(e.code);
+  if (isKey) {
+    switch (e.code) {
+      case 'KeyA': 
+        active[0] = true
+        trigger(0)
+        // notes(true)
+        break;
+      case 'KeyD':
+        active[1] = true
+        trigger(1)
+        break;
+      default: 
+        break; 
+    }
   }
-  console.log(active)
+
 });
 
+// RELEASE PRESS
 document.addEventListener('keyup', (e) => { 
-  let keyCheck = checkIfKey(e.code);
-  if (keyCheck === true) {
-    active[e.code] = false
+  let isKey = checkIfKey(e.code);
+  
+  if (isKey) {
+    switch (e.code) {
+      case 'KeyA': 
+        active[0] = false
+        release(0)
+        // notes(false)
+        break;
+      case 'KeyD':
+        active[1] = false
+        release(1)
+        // notes(false)
+        break;
+      default: 
+        break; 
+    }
   }
-  console.log(active)
 });
-
 
 document.addEventListener('mousemove', (e) => onMouseMoved(e, camera))
-
 
 let particles = {}
 function addParticles() {
@@ -130,7 +185,6 @@ function addParticles() {
   const vertices = [];
 
   for ( let i = 0; i < 500; i ++ ) {
-
     const x = randomInteger(-params.boundary,params.boundary)
     const y = randomInteger(-params.boundary,params.boundary)
     const z = randomInteger(-params.boundary,params.boundary)
@@ -147,70 +201,86 @@ function addParticles() {
   scene.add( particles );
 }
 
-const gridHelper = new THREE.GridHelper( 10, 10 );
-scene.add( gridHelper );
+function trigger (groupId) {
+  console.log('TRIGGERING GROUP' + ' ' + groupId)
+  states[groupId] = 1 // trigger state
+  if (!groups[groupId].children.length) {
+    for (let i=0; i < 100; i++) {
+      generateSphere(groupId)
+    }
+    scene.add(groups[groupId])
+  }
+}
 
-function generateSphere() {
+function release (groupId) {
+  console.log('RELEASING GROUP' + ' ' + groupId)
+  states[groupId] = 2 // release state
+}
+
+function generateSphere(groupId) {
   const geometry  = new THREE.SphereGeometry( params.size, params.height, params.width )
-  const material = new THREE.MeshStandardMaterial({ color: Math.random() * 0xffffff, emissive: 0x0, metalness:0.1, roughness:0.2, opacity:0.2})
-  const id = cubes.length - 1
+  const material = new THREE.MeshStandardMaterial({ 
+    color: Math.random() * 0xffffff, 
+    emissive: 0x0, 
+    metalness:0.1, 
+    roughness:0.2, 
+    transparent:true, 
+    opacity:0.1
+  })
+  
+  // Set to array so we can access from outside scope
+  const id = cubes.length
   cubes[cubes.length] = new THREE.Mesh(geometry, material)
-  const x = randomInteger(-params.boundary,params.boundary)
-  const y = randomInteger(-params.boundary,params.boundary)
-  const z = randomInteger(-params.boundary,params.boundary)
-  cubes[id].position.set(x,y,z)
-  group.add(cubes[id])
 
+  // Set random position
+  const x = randomInteger(-params.boundary, params.boundary)
+  const y = randomInteger(-params.boundary, params.boundary)
+  const z = randomInteger(-params.boundary, params.boundary)
+  cubes[id].position.set(x,y,z)
+
+  // Add to group
+  groups[groupId].add(cubes[id])
+
+  // Add to GUI
   const cube = folder.addFolder(`Cube` + ' ' + id)
   cube.add(cubes[id], 'id')
   cube.add(cubes[id].scale, 'x', 0, 10)
 }
 
-const geometry = new THREE.SphereGeometry( 500, 60, 40 );
-geometry.scale( - 1, 1, 1 );
-
-const texture = new THREE.TextureLoader().load( 'bg.jpeg' );
-const material = new THREE.MeshBasicMaterial( { map: texture } );
-
-const mesh = new THREE.Mesh( geometry, material );
-
-
-function start () {				
-  for (let i=0; i < 100; i++) {
-    const geometry = new THREE.SphereGeometry( params.size, params.height, params.width )
-    const material = new THREE.MeshStandardMaterial({ color: Math.random() * 0xffffff, emissive: 0x0, metalness:0.1, roughness:0.2, opacity:0.2})
-    cubes[i] = new THREE.Mesh(geometry, material)
-    
-    const x = randomInteger(-5,5)
-    const y = randomInteger(-5,5)
-    const z = randomInteger(-5,5)
-
-    cubes[i].position.set(x,y,z)
-    group.add(cubes[i])
-    const cube = folder.addFolder(`Cube` + ' ' + i)
-    cube.add(cubes[i], 'id')
-    cube.add(cubes[i].scale, 'x', 0, 10)
-  }
-
-  // for (let i=0; i < 30; i++) {
-  //   generateSphere()
-  // }
-  scene.add(group)
+function start () {			
+  // Generate initial 100 spheres
   animate()
 }
 
 function animate() {
-  console.log('animate')
-  // scene.fog = new THREE.FogExp2( 0x000000, params.fog );
+  scene.fog = new THREE.FogExp2( 0x000000, params.fog );
 
   if (particles.rotation) {
     particles.rotation.y += input.particles.rotationSpeed
     particles.rotation.x += input.particles.rotationSpeed
   }
 
-  mesh.rotation.y += input.sphere.rotationSpeed
-  group.rotation.x += input.sphere.rotationSpeed
-  group.rotation.y += input.sphere.rotationSpeed
+  states.forEach((state, index) => {
+    // if (groups[index]) {
+    //   groups[index].rotation.x += input.sphere.rotationSpeed
+    //   groups[index].rotation.y += input.sphere.rotationSpeed
+    // }
+    for (let i = 0; i < groups[index].children.length; i++) {
+      let object = groups[index].children[i]
+      if (state == 1) {
+        if (object.material.opacity <= 1) object.material.opacity += 0.005
+      }
+      if (state == 2) {
+        object.material.opacity -= 0.005
+        if (object && object.material.opacity <= 0.01) {
+          // Look at alternative: instead of removing, look to keep low opacity and randomise coordinates if
+          // sphere is already generated
+          groups[index].remove(object)
+        }
+      }
+    }   
+  }) 
+  
   window.requestAnimationFrame(animate)
   composer.render();
 }
